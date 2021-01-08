6b3c54a3581966c08fc62b3ccadd2af7cb6aff6a
fertilewaif <gmkornilov@edu.hse.ru>
2021-01-08 18:39:39 +0300

    added database startup variables as environment variales

--- main.go
+++ main.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/fertilewaif/avito-mx-backend-test/controllers"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	connStr := "user=postgres password=postgres dbname=postgres sslmode=disable"
+const (
+	PORT = 5432
+)
 
-	db, err := sql.Open("postgres", connStr)
+func initDB(username, password, database string) (*sql.DB, error) {
+	dsn := fmt.Sprintf("user = %s port = %d password = %s dbname = %s sslmode=disable",
+		username, PORT, password, database)
+
+	conn, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = conn.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func main() {
+	dbUser, dbPassword, dbName :=
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB")
+
+	db, err := initDB(dbUser, dbPassword, dbName)
 
 	if err != nil {
 		panic(err)
